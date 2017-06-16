f9cd40b3ef50731ca7649c70ae2c3513fc90334f
dmn1k <dominik.schlosser@gmail.com>
2017-06-16 19:45:25 +0200

    Refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<app-wiki-search></app-wiki-search>'
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'app';
